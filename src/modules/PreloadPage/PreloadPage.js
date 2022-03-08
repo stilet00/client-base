9191745c961c946107a3f5b224d47d493dcda1d6
Anton Babchenkov <anton.babchekov@olo.com>
2022-03-08 19:05:02 +0100

    loader in progress

---
+++ src/modules/PreloadPage/PreloadPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import "../../styles/modules/PreloadPage.css"
+import Loader from "../../sharedComponents/Loader/Loader";
+
+function PreloadPage({ isLoaded }) {
+    return (
+        <div className={ isLoaded ? "preload-page" : "preload-page invisible" }>
+            <Loader position={"0"}/>
+        </div>
+    );
+}
+
+export default PreloadPage;
