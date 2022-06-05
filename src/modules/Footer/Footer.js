201558e0b3899844bc4cdbda380925776b3d78a5
stilet00 <safroninanton@gmail.com>
2022-06-05 01:40:52 -0230

    version update

--- src/modules/Footer/Footer.js
+++ src/modules/Footer/Footer.js
@@ -3,7 +3,7 @@ import '../../styles/modules/Footer.css'
 function Footer() {
     return (
         <div className={'footer'}>
-            <p>Made by Stilet 2021 - 2022. Version 2.2.4</p>
+            <p>Made by Stilet 2021 - 2022. Version 2.2.5</p>
         </div>
     )
 }
