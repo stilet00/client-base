d97cd5f2d7354a7feba3b1eecc8127da31b5a003
remind1990 <tigerstylest00@gmail.com>
2022-08-15 14:32:41 +0200

    deleted Search

--- src/modules/Clients/ClientsList/ClientsList.js
+++ src/modules/Clients/ClientsList/ClientsList.js
@@ -1,6 +1,4 @@
 import React from 'react'
-import styled from 'styled-components'
-import { useState } from 'react'
 import Button from '@material-ui/core/Button'
 import Drawer from '@material-ui/core/Drawer'
 import WomanIcon from '@mui/icons-material/Woman'
@@ -37,22 +35,6 @@ function ClientsList({
         calculateMiddleMonthSum,
         getAllAsignedTranslators,
     } = useClientsList(translators)
-    const [Search, setSearch] = useState('')
-    function onSearchChange(e) {
-        setSearch(e.target.value.trim())
-    }
-    function getFiltredClients() {
-        let filtredClients = clients.filter(
-            client =>
-                client.name
-                    .toLowerCase()
-                    .includes(Search.toLocaleLowerCase()) ||
-                client.surname
-                    .toLowerCase()
-                    .includes(Search.toLocaleLowerCase())
-        )
-        return filtredClients || clients
-    }
     return (
         <>
             <Button
@@ -71,91 +53,27 @@ function ClientsList({
                 <div className={'side-clients-menu fallDown-menu'}>
                     <h3>
                         <WomanIcon />
-                        All clients:{' '}
-                        <StyledInputForSearches
-                            type="text"
-                            placeholder="Search..."
-                            defaultValue={Search}
-                            value={Search}
-                            onChange={onSearchChange}
-                        ></StyledInputForSearches>
+                        All clients:
                     </h3>
                     <ul>
-                        {getFiltredClients()
-                            .sort(sortBySum)
-                            .map((client, index) => {
-                                const memoizedMiddleMonthSum =
-                                    calculateMiddleMonthSum(client._id)
-                                const memoizedPreviousMiddleMonthSum =
-                                    calculateMiddleMonthSum(
-                                        client._id,
-                                        moment().subtract(1, 'month')
-                                    )
-                                const memoizedMonthSum = clientMonthSum(
-                                    client._id
-                                )
-                                const memoizedPreviousMonthSum = clientMonthSum(
+                        {clients.sort(sortBySum).map((client, index) => {
+                            const memoizedMiddleMonthSum =
+                                calculateMiddleMonthSum(client._id)
+                            const memoizedPreviousMiddleMonthSum =
+                                calculateMiddleMonthSum(
                                     client._id,
                                     moment().subtract(1, 'month')
                                 )
-                                const progressPage =
-                                    memoizedMiddleMonthSum >=
-                                    memoizedPreviousMiddleMonthSum ? (
-                                        <span>
-                                            {`Middle for ${moment().format(
-                                                'MMMM'
-                                            )}: `}
-                                            <span
-                                                className={
-                                                    'blue-text styled-text-numbers'
-                                                }
-                                            >
-                                                {`${memoizedMiddleMonthSum} $`}
-                                            </span>
-                                            <span
-                                                className={
-                                                    'green-text styled-text-numbers'
-                                                }
-                                            >
-                                                <FontAwesomeIcon
-                                                    icon={faArrowAltCircleUp}
-                                                />
-                                                {` ${calculatePercentDifference(
-                                                    memoizedMiddleMonthSum,
-                                                    memoizedPreviousMiddleMonthSum
-                                                )} %`}
-                                            </span>
-                                        </span>
-                                    ) : (
-                                        <span>
-                                            {`Middle for ${moment().format(
-                                                'MMMM'
-                                            )}: `}
-                                            <span
-                                                className={
-                                                    'blue-text styled-text-numbers'
-                                                }
-                                            >
-                                                {`${memoizedMiddleMonthSum} $`}
-                                            </span>
-                                            <span
-                                                className={
-                                                    'red-text styled-text-numbers'
-                                                }
-                                            >
-                                                <FontAwesomeIcon
-                                                    icon={faArrowAltCircleDown}
-                                                />
-                                                {` ${calculatePercentDifference(
-                                                    memoizedMiddleMonthSum,
-                                                    memoizedPreviousMiddleMonthSum
-                                                )} %`}
-                                            </span>
-                                        </span>
-                                    )
-                                const totalPage = (
+                            const memoizedMonthSum = clientMonthSum(client._id)
+                            const memoizedPreviousMonthSum = clientMonthSum(
+                                client._id,
+                                moment().subtract(1, 'month')
+                            )
+                            const progressPage =
+                                memoizedMiddleMonthSum >=
+                                memoizedPreviousMiddleMonthSum ? (
                                     <span>
-                                        {`Balance for ${moment().format(
+                                        {`Middle for ${moment().format(
                                             'MMMM'
                                         )}: `}
                                         <span
@@ -163,84 +81,134 @@ function ClientsList({
                                                 'blue-text styled-text-numbers'
                                             }
                                         >
-                                            {`${memoizedMonthSum} $`}
+                                            {`${memoizedMiddleMonthSum} $`}
+                                        </span>
+                                        <span
+                                            className={
+                                                'green-text styled-text-numbers'
+                                            }
+                                        >
+                                            <FontAwesomeIcon
+                                                icon={faArrowAltCircleUp}
+                                            />
+                                            {` ${calculatePercentDifference(
+                                                memoizedMiddleMonthSum,
+                                                memoizedPreviousMiddleMonthSum
+                                            )} %`}
                                         </span>
                                     </span>
-                                )
-
-                                const previousTotalPage = (
+                                ) : (
                                     <span>
-                                        {`Balance for ${moment()
-                                            .subtract(1, 'month')
-                                            .format('MMMM')}: `}
+                                        {`Middle for ${moment().format(
+                                            'MMMM'
+                                        )}: `}
                                         <span
                                             className={
                                                 'blue-text styled-text-numbers'
                                             }
                                         >
-                                            {`${memoizedPreviousMonthSum} $`}
+                                            {`${memoizedMiddleMonthSum} $`}
+                                        </span>
+                                        <span
+                                            className={
+                                                'red-text styled-text-numbers'
+                                            }
+                                        >
+                                            <FontAwesomeIcon
+                                                icon={faArrowAltCircleDown}
+                                            />
+                                            {` ${calculatePercentDifference(
+                                                memoizedMiddleMonthSum,
+                                                memoizedPreviousMiddleMonthSum
+                                            )} %`}
                                         </span>
                                     </span>
                                 )
-                                return (
-                                    <li
-                                        key={client._id}
-                                        id={client._id}
-                                        className={'side-clients-menu__client'}
-                                        draggable={true}
-                                        onDragStart={e =>
-                                            dragStartHandler(e, client)
+                            const totalPage = (
+                                <span>
+                                    {`Balance for ${moment().format('MMMM')}: `}
+                                    <span
+                                        className={
+                                            'blue-text styled-text-numbers'
                                         }
-                                        onDragOver={dragOverHandler}
-                                        onDragLeave={dragLeaveHandler}
-                                        onDragEnd={dragEndHandler}
-                                        onDrop={e => dragDropHandler(e)}
                                     >
-                                        <ListItemText
-                                            primary={`${client.name} ${client.surname}`}
-                                        />
-                                        <Rating
-                                            name="read-only"
-                                            value={getClientsRating(client._id)}
-                                            readOnly
-                                            size="small"
-                                        />
-                                        {/*<Button onClick={() => deleteClient(client._id)} disabled>*/}
-                                        {/*  <DeleteForeverIcon />*/}
-                                        {/*</Button>*/}
-                                        <ListItemText
-                                            className={
-                                                'side-clients-menu__client__balance-container'
-                                            }
-                                            secondary={totalPage}
-                                        ></ListItemText>
-                                        <ListItemText
-                                            className={
-                                                'side-clients-menu__client__balance-container'
-                                            }
-                                            secondary={progressPage}
-                                        />
-                                        <ListItemText
-                                            className={
-                                                'side-clients-menu__client__balance-container'
-                                            }
-                                            secondary={previousTotalPage}
-                                        />
-                                        <ListItemText
-                                            className={
-                                                'side-clients-menu__client__balance-container'
-                                            }
-                                            secondary={`Assigned at ${
-                                                getAllAsignedTranslators(
-                                                    client._id
-                                                ).length
-                                            } translators: ${getAllAsignedTranslators(
-                                                client._id
-                                            ).join(', ')}`}
-                                        />
-                                    </li>
-                                )
-                            })}
+                                        {`${memoizedMonthSum} $`}
+                                    </span>
+                                </span>
+                            )
+
+                            const previousTotalPage = (
+                                <span>
+                                    {`Balance for ${moment()
+                                        .subtract(1, 'month')
+                                        .format('MMMM')}: `}
+                                    <span
+                                        className={
+                                            'blue-text styled-text-numbers'
+                                        }
+                                    >
+                                        {`${memoizedPreviousMonthSum} $`}
+                                    </span>
+                                </span>
+                            )
+                            return (
+                                <li
+                                    key={client._id}
+                                    id={client._id}
+                                    className={'side-clients-menu__client'}
+                                    draggable={true}
+                                    onDragStart={e =>
+                                        dragStartHandler(e, client)
+                                    }
+                                    onDragOver={dragOverHandler}
+                                    onDragLeave={dragLeaveHandler}
+                                    onDragEnd={dragEndHandler}
+                                    onDrop={e => dragDropHandler(e)}
+                                >
+                                    <ListItemText
+                                        primary={`${client.name} ${client.surname}`}
+                                    />
+                                    <Rating
+                                        name="read-only"
+                                        value={getClientsRating(client._id)}
+                                        readOnly
+                                        size="small"
+                                    />
+                                    {/*<Button onClick={() => deleteClient(client._id)} disabled>*/}
+                                    {/*  <DeleteForeverIcon />*/}
+                                    {/*</Button>*/}
+                                    <ListItemText
+                                        className={
+                                            'side-clients-menu__client__balance-container'
+                                        }
+                                        secondary={totalPage}
+                                    ></ListItemText>
+                                    <ListItemText
+                                        className={
+                                            'side-clients-menu__client__balance-container'
+                                        }
+                                        secondary={progressPage}
+                                    />
+                                    <ListItemText
+                                        className={
+                                            'side-clients-menu__client__balance-container'
+                                        }
+                                        secondary={previousTotalPage}
+                                    />
+                                    <ListItemText
+                                        className={
+                                            'side-clients-menu__client__balance-container'
+                                        }
+                                        secondary={`Assigned at ${
+                                            getAllAsignedTranslators(client._id)
+                                                .length
+                                        } translators: ${getAllAsignedTranslators(
+                                            client._id
+                                        ).join(', ')}`}
+                                    />
+                                </li>
+                            )
+                        })}
                     </ul>
                 </div>
             </Drawer>
@@ -249,14 +217,3 @@ function ClientsList({
 }
 
 export default ClientsList
-
-const StyledInputForSearches = styled.input`
-    padding: 0;
-    background: transparent;
-    border: none;
-    font-family: inherit;
-    font-size: inherit;
-    color: inherit;
-    margin-left: 1rem;
-    outline: none;
-`
