6996d875316e8e25fe57adf6e1780ec5723a04b9
stilet00 <safroninanton@gmail.com>
2022-08-27 11:43:57 -0230

    Adding version as environment variable, making footer more readable

--- src/modules/Footer/Footer.js
+++ src/modules/Footer/Footer.js
@@ -1,9 +1,14 @@
 import React from 'react'
 import '../../styles/modules/Footer.css'
+import moment from 'moment'
 function Footer() {
     return (
         <div className={'footer'}>
-            <p>Made by Stilet 2021 - 2022. Version 2.3.0</p>
+            <p>
+                {`Made by Stilet 2021 - ${moment().format('YYYY')}. Version ${
+                    process.env.REACT_APP_SUNRISE_AGENCY_VERSION
+                }`}
+            </p>
         </div>
     )
 }
