c2c56f3c4066c0fc02d61781165be7370d1222bd
remind1990 <tigerstylest00@gmail.com>
2022-08-29 19:59:56 +0200

    first commit

--- src/modules/Payments/Payments.js
+++ src/modules/Payments/Payments.js
@@ -1,9 +1,8 @@
-import React from 'react'
-import Loader from '../../sharedComponents/Loader/Loader'
+import { useState } from 'react'
 import SinglePayment from './SinglePayment/SinglePayment'
 import Form from './Form/Form'
-import { usePaymentsList } from './businessLogic'
 import styled from 'styled-components'
+import moment from 'moment'
 
 const Button = styled.button`
     width: 30%;
@@ -18,31 +17,62 @@ const Button = styled.button`
         background: rgb(192 190 190);
     }
 `
+const Wrapper = styled.div`
+    width: 100%;
+    padding: 0 1rem;
+    background: rgba(255, 255, 255, 1);
+    box-sizing: border-box;
+`
+
+const DataHeader = styled.div`
+    background: rgba(255, 255, 255, 1);
+    padding: 5px 0;
+    text-align: left;
+    width: 100%;
+    border-bottom: 1px solid rgb(224, 224, 224);
+    color: rgb(51, 51, 51, 1);
+    font-size: 0.875rem;
+`
+
+const CategoryHolder = styled.div`
+    margin: 1rem 0;
+    width: 100%;
+    display: flex;
+    justify-content: space-evenly;
+`
+
 export default function Payments() {
-    const { paymentsList, loading, newPayment } = usePaymentsList()
-    const page =
-        paymentsList.length && !loading ? (
-            paymentsList.map(item => <SinglePayment key={item.id} {...item} />)
-        ) : loading ? (
-            <Loader />
-        ) : (
-            <h1>No payments yet</h1>
-        )
+    const [paymentsList, setPaymentsList] = useState([
+        {
+            name: 'Ivanova Anna',
+            amount: '150',
+            sender: 'Agency',
+            comments: 'monthly payment',
+            date: moment().format('MMM Do YY'),
+        },
+        {
+            name: 'Steian Andrea',
+            amount: '150',
+            sender: 'Anton',
+            comments: 'Payed to scount',
+            date: moment().format('MMM Do YY'),
+        },
+        {
+            name: 'Bavdis Mariana',
+            amount: '3159',
+            sender: 'Agency',
+            comments: 'Salary for July 2022',
+            date: moment().format('MMM Do YY'),
+        },
+    ])
+
     return (
         <>
             <div className={'main-container chart-container animated-box'}>
-                <div
-                    className={'category-holder'}
-                    style={{
-                        margin: '1rem 0',
-                        width: '100%',
-                        display: 'flex',
-                        justifyContent: 'space-evenly',
-                    }}
-                >
+                <CategoryHolder>
                     <Button>CLIENTS</Button>
                     <Button>TRANSLATORS</Button>
-                </div>
+                </CategoryHolder>
 
                 <ul
                     className={'scrolled-container'}
@@ -52,35 +82,16 @@ export default function Payments() {
                         overflow: 'auto',
                     }}
                 >
-                    <div
-                        className={'ul-wrapper'}
-                        style={{
-                            width: '100%',
-                            padding: '0 1rem',
-                            background: 'rgba(255, 255, 255, 1)',
-                            boxSizing: 'border-box',
-                        }}
-                    >
-                        <div
-                            className={'data-header'}
-                            style={{
-                                background: 'rgba(255, 255, 255, 1)',
-                                padding: '5px 0',
-                                textAlign: 'left',
-                                width: '100%',
-                                borderBottom: '1px solid rgb(224, 224, 224) ',
-                                color: 'rgb(51, 51, 51, 1)',
-                                fontSize: '0.875rem',
-                            }}
-                        >
-                            29.08.2022
-                        </div>
-                        {page}
-                    </div>
+                    <Wrapper>
+                        <DataHeader>29.08.2022</DataHeader>
+                        {paymentsList.map(item => (
+                            <SinglePayment {...item} />
+                        ))}
+                    </Wrapper>
                 </ul>
             </div>
             <div className="socials button-add-container bottom-button">
-                <Form createPayment={newPayment} />
+                <Form />
             </div>
         </>
     )
