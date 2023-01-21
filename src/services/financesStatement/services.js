d41e6f1a661d1263b8c1c63ecfaf59a6fcdfc5c3
stilet00 <safroninanton@gmail.com>
2023-01-21 19:46:34 -0330

    Adding authorization layer

--- src/services/financesStatement/services.js
+++ src/services/financesStatement/services.js
@@ -1,16 +1,27 @@
 import axios from 'axios'
 import { rootURL } from '../rootURL'
+import { getConfigForAxiosAuthenticatedRequest } from '../utils'
 
 const financeStatementsURL = rootURL + 'statements/'
 
 export function getPaymentsRequest() {
-    return axios.get(financeStatementsURL + 'get/')
+    return axios.get(
+        financeStatementsURL + 'get/',
+        getConfigForAxiosAuthenticatedRequest()
+    )
 }
 
 export function addPaymentRequest(payment) {
-    return axios.post(financeStatementsURL + 'add/', payment)
+    return axios.post(
+        financeStatementsURL + 'add/',
+        payment,
+        getConfigForAxiosAuthenticatedRequest()
+    )
 }
 
 export function removePaymentRequest(id) {
-    return axios.delete(financeStatementsURL + id)
+    return axios.delete(
+        financeStatementsURL + id,
+        getConfigForAxiosAuthenticatedRequest()
+    )
 }
