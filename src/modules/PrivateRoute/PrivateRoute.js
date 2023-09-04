6880f5e4bb4197662f21108f168d5a050508aa67
stilet00 <safroninanton@gmail.com>
2023-09-04 17:30:48 -0230

    Rename to private route

--- src/modules/PrivateRoute/PrivateRoute.js
+++ src/modules/PrivateRoute/PrivateRoute.js
@@ -1,14 +1,20 @@
 import React from 'react'
-import { Redirect } from 'react-router-dom'
+import { Redirect, Route } from 'react-router-dom'
 import { useAdminStatus } from '../../sharedHooks/useAdminStatus'
 import Loader from '../../sharedComponents/Loader/Loader' // Adjust the path as needed
 
-export default function AsyncComponentRenderer({ user, component: Component }) {
+export default function PrivateRoute({ user, component: Component, path }) {
     const admin = useAdminStatus(user)
 
     if (admin === null) {
         return <Loader />
     }
 
-    return admin ? <Component user={user} /> : <Redirect to="/" />
+    return admin ? (
+        <Route path={path}>
+            <Component user={user} />
+        </Route>
+    ) : (
+        <Redirect to="/" />
+    )
 }
