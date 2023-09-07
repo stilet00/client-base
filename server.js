ce0e7bfd32d32b548e39cebe85274baa8d91a306
stilet00 <safroninanton@gmail.com>
2023-09-07 02:54:43 -0230

    revert back body parser limit

--- server.js
+++ server.js
@@ -54,8 +54,8 @@ const limiter = rateLimit({
 
 app.use(express.static(__dirname + '/build'))
 app.set('view engine', 'ejs')
-app.use(bodyParser.json({ limit: '100mb' }))
-app.use(bodyParser.urlencoded({ limit: '100mb', extended: true }))
+app.use(bodyParser.json({ limit: '50mb' }))
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }))
 app.use(function (request, response, next) {
     response.setHeader('Access-Control-Allow-Origin', '*')
     response.setHeader(
