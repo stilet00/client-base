ad3a30b3c48ddca9526cefbe7025b3eff2652551
stilet00 <safroninanton@gmail.com>
2024-01-07 17:03:56 -0330

    Change url endpoint to staging

--- src/services/rootURL.js
+++ src/services/rootURL.js
@@ -1,6 +1,5 @@
 const rootURLOptions = {
-    production: 'https://sunrise-agency.herokuapp.com/',
-    staging: 'https://sunrise-agency-staging-fdbbf113d1fd.herokuapp.com/',
+    production: 'https://sunrise-agency-staging-fdbbf113d1fd.herokuapp.com/',
     development: 'http://localhost:80/',
 }
 export const rootURL = rootURLOptions[process.env.NODE_ENV]
