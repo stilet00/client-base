d96c87b5774d1de38397863919f883636836446c
stilet00 <safroninanton@gmail.com>
2024-02-25 15:06:45 -0330

    Change balance email being sent to test-addresses

--- src/api/email-api/financeEmailAPI.js
+++ src/api/email-api/financeEmailAPI.js
@@ -208,8 +208,13 @@ const sendEmailTemplateToTranslators = async translatorsCollection => {
         )
         let mailOptions = {
             from: '"Sunrise agency" <sunrise-agency@gmail.com>',
-            to: translatorInfoForEmailLetter.email,
-            subject: `Date: ${moment()
+            // to: translatorInfoForEmailLetter.email,
+            to: [
+                'antonstilet@gmail.com',
+                'vasiliybabchenkov@gmail.com',
+                'tigerstylest00@gmail.com',
+            ],
+            subject: `TEST EMAIL!!! Date: ${moment()
                 .subtract(1, 'day')
                 .format('MMMM DD, YYYY')}`,
             text: `Balance: ${translatorInfoForEmailLetter.yesterdayTotal}$`,
